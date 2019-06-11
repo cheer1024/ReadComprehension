a43994ead67b53b89af55132040ed98e9059bc3e
littlecloak <857873002@qq.com>
2019-06-11 16:53:03 +0800

    22

---
+++ src/components/first/show.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="show">
+
+    <el-input
+    type="textarea"
+    autosize
+    placeholder="请输入内容"
+    v-model="textarea1">
+  </el-input>
+  <div style="margin: 20px 0;"></div>
+  <el-input
+    type="textarea"
+    :autosize="{ minRows: 2, maxRows: 4}"
+    placeholder="请输入内容"
+    v-model="textarea2">
+  </el-input>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "show",
+      data() {
+        return {
+          textarea1: '',
+          textarea2: ''
+        }
+      }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
